227c6a69d66a19f54cd50549b05849cd2f6c0e79
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2023-10-07 22:45:10 -0400

    fixed left and right movement

--- Pygame_tut/tut.py
+++ Pygame_tut/tut.py
@@ -70,15 +70,16 @@ class Game:
                     sys.exit()
 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.movement[0] = True
                     if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.movement[0] = False
                     if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
+                
                         
             
             pygame.display.update()
