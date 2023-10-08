0e49dea20ae35d80993f9d693b92173d50c18fc1
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-10-07 23:32:47 -0400

    overwrite

--- something/scripts/utils.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-BASE_IMG_PATH = 'Pygame_tut/assets/'
-
-def load_image(path):
-    # .convert makes image more optimal for pygame
-    img = pygame.image.load(BASE_IMG_PATH + path).convert()
-    img.set_colorkey ((0,0,0))
-    return img
