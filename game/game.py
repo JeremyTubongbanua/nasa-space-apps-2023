a57d48b841b284b5fe7d4bbc367b21927cc91f2f
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-10-07 23:30:36 -0400

    chore: format

--- game/game.py
+++ game/game.py
@@ -6,6 +6,7 @@ from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
 from scripts.tilemap import Tilemap
 
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -15,9 +16,9 @@ class Game:
         self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
-        
+
         self.movement = [False, False]
-        
+
         self.assets = {
             'decor': load_images('tiles/decor'),
             'grass': load_images('tiles/grass'),
@@ -25,20 +26,21 @@ class Game:
             'stone': load_images('tiles/stone'),
             'player': load_image('entities/player.png')
         }
-        
+
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
-        
+
         self.tilemap = Tilemap(self, tile_size=16)
-        
+
     def run(self):
         while True:
             self.display.fill((14, 219, 248))
-            
+
             self.tilemap.render(self.display)
-            
-            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+
+            self.player.update(
+                self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
-            
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -55,9 +57,11 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-            
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+
+            self.screen.blit(pygame.transform.scale(
+                self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
+
 
 Game().run()
